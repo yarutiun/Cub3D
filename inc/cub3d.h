2b674df615b99c4fd341041446f20905dcbcc0b7
yarutiun <y.arutiunofff@gmail.com>
2023-04-09 23:19:58 +0200

    inc file is created, libraries are connected

---
+++ inc/cub3d.h
@@ -0,0 +1,12 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include "../mlx/mlx.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+//we should for sure include libft
+
+#endif

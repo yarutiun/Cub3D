e5af5f1aa5505933e36c90bd9b2fa9211e5c8fa9
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-12 09:33:40 +0200

    Add structure and templates

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,12 +1,34 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include "../mlx/mlx.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
+/* Macros */
 
-//we should for sure include libft
+
+/* Libraries */
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+# include <stdio.h>
+
+# include "../mlx/mlx.h"
+# include "../libft/libft.h"
+
+/* Structs */
+typedef struct s_square
+{
+	struct s_cube		*cube;
+	int					area;
+}						t_square;
+
+typedef struct s_cube
+{
+	t_square			*squares;
+	int					volume;	
+}						t_cube;
+
+/* Functions */
+// Parse Arguments //
+
+// Utils //
 
 #endif

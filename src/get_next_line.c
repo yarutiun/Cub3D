8cf2408a6578a873b2c30c5f83ac92c00413d14f
yarutiun <y.arutiunofff@gmail.com>
2023-04-14 14:25:25 +0200

    Remove 42 header

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -1,38 +1,30 @@
-#include "../inc/cub3d.h"
+#include "cub3d.h"
 
-bool	free_get_next_line(char *buff, int i, int readed)
+char *get_next_line(int fd)
 {
-	if ((!buff[i - 1] && !readed) || readed == -1)
-	{
-		free(buff);
-		return (true);
-	}
-	return (false);
-}
-
-char	*get_next_line(int fd)
-{
-	int		i;
-	int		readed;
-	char	c;
-	char	*buff;
+	int i;
+	int readed;
+	char c;
+	char *buff;
 
 	i = 0;
 	buff = malloc(sizeof(char) * 100);
-	while (1)
+	while((readed = read(fd, &c, 1)) > 0)
 	{
-		readed = read(fd, &c, 1);
-		if (readed > 0)
-		{
-			buff[i++] = c;
-			if (c == '\n')
-				break ;
-		}
-		else
-			break ;
+		buff[i++] = c;
+		if(c == '\n')
+			break;
 	}
-	if (free_get_next_line(buff, i, readed))
-		return (NULL);
+	if((!buff[i - 1] && !readed) || readed == -1)
+	{
+		free(buff);
+		return(NULL);
+	}
+	if(buff[i - 1] == '\n')
+	{
+		buff[i - 1] = '\0';
+		return(buff);	
+	}
 	buff[i] = '\0';
-	return (buff);
+	return(buff);
 }

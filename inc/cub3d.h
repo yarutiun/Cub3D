88950f4889594e776eea9f36434274b436e9a0be
yarutiun <y.arutiunofff@gmail.com>
2023-04-14 14:24:23 +0200

    Add cube struct

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -14,25 +14,28 @@
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
-# endif
-
 /* Structs */
-typedef struct s_square
+typedef struct s_param
 {
 	struct s_cube		*cube;
-	int					area;
-}						t_square;
+	char				**file_input;
+	char				*no;
+	char				*so;
+	char				*we;
+	char				*ea;
+	char				*f;
+	char				*c;
+	char 				**map;
+}						t_param;
 
 typedef struct s_cube
 {
-	t_square			*squares;
-	int					volume;	
+	t_param				*param;
 }						t_cube;
 
 /* Functions */
 // Parser //
+int		parser(void);
 int		check_extension(const char *file_name);
 char	**convert_map(char *file);
 
@@ -40,5 +43,8 @@ char	**convert_map(char *file);
 char	*read_file(int fd, char *stash);
 char	*get_return(char *stash);
 char	*get_next_line(int fd);
+
+// Errors //
+int		parsing_err(void);
 
 #endif

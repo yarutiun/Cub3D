e03f07ab159519894524d23fe887798b03616fc6
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-16 22:28:31 +0200

    Add free_ptr func

--- src/free.c
+++ src/free.c
@@ -1,15 +1,20 @@
 #include "cub3d.h"
 
 
-void	free_double_char_ptr(char **arr)
+void 	free_ptr(void *ptr, void free_func(void *))
+{
+	if(ptr)
+		free_func(ptr);
+}
+
+void	free_double_char_ptr(void *arr_p)
 {
 	int	i;
-
+	char **arr = arr_p;
 	i = 0;
 	while (arr[i])
 	{
-		if (arr[i])
-			free(arr[i]);
+		free(arr[i]);
 		arr[i] = NULL;
 		i++;
 	}
@@ -18,30 +23,15 @@ void	free_double_char_ptr(char **arr)
 
 void	free_param(t_param *param)
 {
-	if (param->input_str)
-		free(param->input_str);
+	free_ptr(param->input_str, free);
 	param->input_str = NULL;
-	if (param->splitted_input)
-		free_double_char_ptr(param->splitted_input);
-	param->splitted_input = NULL;
-	if (param->no)
-		free(param->no);
-	param->no = NULL;
-	if (param->so)
-		free(param->so);
-	param->so = NULL;
-	if (param->we)
-		free(param->we);
-	param->we = NULL;
-	if (param->ea)
-		free(param->ea);
-	param->ea = NULL;
-	if (param->f)
-		free(param->f);
-	param->f = NULL;
-	if (param->c)
-		free(param->c);
-	param->c = NULL;
+	free_ptr(param->splitted_input, free_double_char_ptr);
+	free_ptr(param->no, free);
+	free_ptr(param->so, free);
+	free_ptr(param->we, free);
+	free_ptr(param->ea, free);
+	free_ptr(param->f, free);
+	free_ptr(param->c, free);
 }
 
 void	free_all(t_cube *cube)

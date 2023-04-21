999d39634f1e9b1202773a95a6a80cc1a89777ae
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-21 15:47:25 +0200

    Need to test intel based free

--- src/graphics/window.c
+++ src/graphics/window.c
@@ -1,15 +1,16 @@
 #include "cub3d.h"
 
-int	x_close(void)
+int	x_close(t_cube *cube)
 {
-	
+	// (void)cube;
+	free_all(cube);
 	exit(EXIT_SUCCESS);
 }
 
-int	key_hooks(int keycode)
+int	key_hooks(int keycode, t_cube *cube)
 {
 	if (keycode == 53)
-		x_close();
+		x_close(cube);
 	return (0);
 }
 
@@ -26,8 +27,8 @@ void	launch_mlx(t_cube *cube)
 	img->address = mlx_get_data_addr(img->img, &img->bits_per_pixel, \
 									&img->line_length, &img->endian);
 	// raycasting(cube);
-	mlx_hook(mlx->window, ESCAPE, 0, x_close, 0);
-	mlx_key_hook(mlx->window, key_hooks, 0);
+	mlx_hook(mlx->window, ESCAPE, 0, &x_close, &cube);
+	mlx_key_hook(mlx->window, &key_hooks, &cube);
 	mlx_do_sync(mlx->mlx_ptr);
 	mlx_loop(mlx->mlx_ptr);
 }

a00cf875865685f909203381490c97c87ddf224f
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-22 10:53:06 +0200

    Add add_pixel_color func

--- src/graphics/raycasting.c
+++ src/graphics/raycasting.c
@@ -4,17 +4,41 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	if (x < WIDTH && y < HEIGHT && x > 0 && y > 0)
+	dst = img->address + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	add_pixel_color(t_cube *cube, int x, int y)
+{
+	int color = 0x000066FF;
+
+	my_mlx_pixel_put(&cube->img, x, y, color);
+}
+
+void	draw_vertical_lines(t_cube *cube)
+{
+	int	pixel_x;
+	int	pixel_y;
+
+	pixel_x = cube->img.pixel_x;
+	pixel_y = cube->img.pixel_y;
+	pixel_x = 0;
+	while (pixel_x < WIDTH)
 	{
-		dst = img->address + (y * img->line_length + x * (img->bits_per_pixel / 8));
-		*(unsigned int *)dst = color;
+		pixel_y = 0;
+		while (pixel_y < HEIGHT)
+		{
+			add_pixel_color(cube, pixel_x, pixel_y);
+			pixel_y++;
+		}
+		pixel_x++;
 	}
 }
 
-void	raycasting(t_cube *cube)
+void	render_window(t_cube *cube)
 {
 	mlx_clear_window(cube->mlx.mlx_ptr, cube->mlx.window);
-	//draw_vertical_lines(cube); //From left to right, form bottom to top
+	draw_vertical_lines(cube); //From left to right, form bottom to top
 	mlx_put_image_to_window(cube->mlx.mlx_ptr, cube->mlx.window, \
 							cube->img.img, 0, 0);
 }

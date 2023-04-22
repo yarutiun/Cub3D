e487c171e8bfb1c5a5bb36d239cffe054f4b68bc
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-22 10:51:46 +0200

    Add get_next_line to utils

--- src/utils.c
+++ src/utils.c
@@ -11,3 +11,32 @@ int     array_size(char **arr)
     }
     return (i);
 }
+
+char *get_next_line(int fd)
+{
+	int i;
+	int readed;
+	char c;
+	char *buff;
+
+	i = 0;
+	buff = malloc(sizeof(char) * 100);
+	while((readed = read(fd, &c, 1)) > 0)
+	{
+		buff[i++] = c;
+		if(c == '\n')
+			break;
+	}
+	if((!buff[i - 1] && !readed) || readed == -1)
+	{
+		free(buff);
+		return(NULL);
+	}
+	if(buff[i - 1] == '\n')
+	{
+		buff[i - 1] = '\0';
+		return(buff);	
+	}
+	buff[i] = '\0';
+	return(buff);
+}

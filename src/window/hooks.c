400c310fb0f3ebb8e55d7b20566dcf34d5ff855c
Santiago Tena Hernandez <stenahe@gmail.com>
2023-04-29 12:26:21 +0200

    Return to previous nesting

--- src/window/hooks.c
+++ src/window/hooks.c
@@ -27,27 +27,31 @@ int	key_hooks(int keycode, t_rc *rc)
 	double	factor;
 
 	map = rc->cube->param.map;
-	rotation_speed = 0.2;
+	rotation_speed = 0.15;
 	move_speed = rotation_speed * (5 / 3);
-	factor = move_speed * 1;
+	factor = move_speed * 1.25;
 	if (keycode == ESCAPE_KEY)
 		x_close();
 	else if (keycode == W_KEY)
 	{
 		if (map[(int)(rc->position.x + rc->direction.x * factor)][(int)rc->position.y] == '0')
 		{
-			rc->position.x += rc->direction.x * move_speed;
 			if (map[(int)rc->position.x][(int)(rc->position.y + rc->position.y * factor)] == '0')
+			{
+				rc->position.x += rc->direction.x * move_speed;
 				rc->position.y += rc->direction.y * move_speed;
+			}
 		}
 	}
 	else if (keycode == S_KEY)
 	{
 		if (map[(int)(rc->position.x - rc->direction.x * factor)][(int)rc->position.y] == '0')
 		{
-			rc->position.x -= rc->direction.x * move_speed;
 			if (map[(int)rc->position.x][(int)(rc->position.y - rc->position.y * factor)] == '0')
+			{
+				rc->position.x -= rc->direction.x * move_speed;
 				rc->position.y -= rc->direction.y * move_speed;
+			}
 		}
 	}
 	if (keycode == A_KEY)
